1bf1b36094c221511b70112e0a759e3d94065936
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-07-23 15:38:17 -0600

    added service spec, adding offline and dynamodb local plugins

--- backend/src/dynamodb/utils/get-client.ts
+++ backend/src/dynamodb/utils/get-client.ts
@@ -1,7 +1,7 @@
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
 export function createDynamoDBClient() {
-  if (process.env.IS_OFFLINE) {
+  if (process.env.IS_OFFLINE || process.env.JEST_WORKER_ID) {
     console.log('Creating a local DynamoDB instance')
     return new DocumentClient({
       region: 'localhost',
