09c6b12b3675862d89e4fc9ad8512689c08bdaff
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-07-24 14:19:07 -0600

    remove unnecessary s3 mock

--- backend/__mocks__/aws-sdk/clients/s3.ts
+++
@@ -1,8 +0,0 @@
-export const awsSdkPromiseResponse = jest.fn().mockReturnValue(Promise.resolve(true));
-
-const deleteObjectMock = jest.fn().mockImplementation(() => ({ promise: awsSdkPromiseResponse }));
-
-export class S3 {
-  getSignedUrl = jest.fn();
-  deleteObject = deleteObjectMock;
-}
