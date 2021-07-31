b0fd786682dfc275c8d3529d7371feacbc6207b0
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-07-31 11:25:32 -0600

    adding env vars

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: 'dev-dl7xpde6.us.auth0.com',            // Auth0 domain
-  clientId: 'K4LSHbRPXGnlRkLpCm2P5H3VN1w88aUB',          // Auth0 client id
+  domain: process.env.REACT_APP_AUTH0_DOMAIN,            // Auth0 domain
+  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
